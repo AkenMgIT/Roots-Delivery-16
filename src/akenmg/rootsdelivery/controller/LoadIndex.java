779326bb43f9377a47d83e3b9809dc6cce045df2
AkenMg <akenmg.it@gmail.com>
2016-01-12 20:37:40 +0100

    test from omega computer

--- src/akenmg/rootsdelivery/controller/LoadIndex.java
+++ src/akenmg/rootsdelivery/controller/LoadIndex.java
@@ -29,6 +29,7 @@ public class LoadIndex extends HttpServlet {
 		// TODO Auto-generated method stub
 //		response.getWriter().append("Served at: ").append(request.getContextPath());
 		response.getWriter().println("<h1>LoadIndex Servlet</h1>");
+		response.getWriter().println("<h1>Say Hello From Omega</h1>");
 	}
 
 	/**
